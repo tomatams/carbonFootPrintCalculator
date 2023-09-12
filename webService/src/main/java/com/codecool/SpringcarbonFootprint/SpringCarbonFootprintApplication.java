dcc86eba4d8c35e069500a55642aa3230412852e
tomatams <tomatams@gmail.com>
2023-09-12 11:26:15 +0200

    start spring app and create gitignore

---
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/SpringCarbonFootprintApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.SpringcarbonFootprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCarbonFootprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCarbonFootprintApplication.class, args);
+	}
+
+}
