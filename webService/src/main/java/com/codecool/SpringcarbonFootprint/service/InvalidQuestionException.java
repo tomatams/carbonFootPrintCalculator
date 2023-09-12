48634c7067d577f69bb9c31144e41dc6958a898e
tomatams <tomatams@gmail.com>
2023-09-12 14:59:13 +0200

    create InvalidQuestionException

---
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/service/InvalidQuestionException.java
@@ -0,0 +1,9 @@
+package com.codecool.SpringcarbonFootprint.service;
+
+public class InvalidQuestionException extends Exception {
+
+    public InvalidQuestionException(String error) {
+        super(error);
+    }
+
+}
