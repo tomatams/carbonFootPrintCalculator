944c794fe605741df70c451763c35d60597862be
tomatams <tomatams@gmail.com>
2023-09-12 15:04:04 +0200

    delete AnswerRepository.java

--- webService/src/main/java/com/codecool/SpringcarbonFootprint/repository/AnswerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codecool.SpringcarbonFootprint.repository;
-
-import com.codecool.SpringcarbonFootprint.model.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-}
