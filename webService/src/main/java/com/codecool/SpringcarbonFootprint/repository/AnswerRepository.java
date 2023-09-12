42ab54d4c6b79aeaa42bfb8d65f84d545de9c213
tomatams <tomatams@gmail.com>
2023-09-12 14:58:43 +0200

    create AnswerRepository

---
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/repository/AnswerRepository.java
@@ -0,0 +1,9 @@
+package com.codecool.SpringcarbonFootprint.repository;
+
+import com.codecool.SpringcarbonFootprint.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
