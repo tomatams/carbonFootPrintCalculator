f658a8186704dff0f383190b7fd6056b6429a7f6
tomatams <tomatams@gmail.com>
2023-09-13 16:13:00 +0200

    add notfoudQuestionException

---
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/service/NotFoundQuestionException.java
@@ -0,0 +1,8 @@
+package com.codecool.SpringcarbonFootprint.service;
+
+public class NotFoundQuestionException extends Exception {
+
+    public NotFoundQuestionException(String error) {
+        super(error);
+    }
+}
