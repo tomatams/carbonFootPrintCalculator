ce267b47bbf4e655ba7b374e5f2d8552eb78b22b
tomatams <tomatams@gmail.com>
2023-09-14 09:38:24 +0200

    add a file with backend endpoints

---
+++ frontend/src/api/backendRoutes.js
@@ -0,0 +1,8 @@
+const backendRoutes = {
+    getAllQuestions: "/questions/all",
+    getOneQuestion: "/questions/all",
+    postOneQuestion: "/questions/add",
+    deleteOneQuestion: "/questions/delete/"
+  };
+  
+  export default backendRoutes;
