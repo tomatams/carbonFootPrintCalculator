2873e6f3c7121d6de061c99e692c4cf4540aec40
tomatams <tomatams@gmail.com>
2023-09-14 13:28:10 +0200

    modify to have nice css

--- frontend/src/Components/CarbonFootprintForm/Answer.jsx
+++ frontend/src/Components/CarbonFootprintForm/Answer.jsx
@@ -1,8 +1,8 @@
 const Answer = ({answer, onChose}) => {
 
     return (
-        <div >
-            <button className="fpf-answer-button"onClick={()=>onChose(answer.id)}>{answer.answer}</button>
+        <div className="fpf-answer-button">
+            <button  onClick={()=>onChose(answer.id)}>{answer.answer}</button>
         </div>
     )
 }
