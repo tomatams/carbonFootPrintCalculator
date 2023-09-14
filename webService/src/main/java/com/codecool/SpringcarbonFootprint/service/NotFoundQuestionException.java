c7686202dadce007f54b16824578c26c6bb714ff
tomatams <tomatams@gmail.com>
2023-09-14 08:06:15 +0200

    delete unused rows

--- webService/src/main/java/com/codecool/SpringcarbonFootprint/service/NotFoundQuestionException.java
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/service/NotFoundQuestionException.java
@@ -1,7 +1,6 @@
 package com.codecool.SpringcarbonFootprint.service;
 
 public class NotFoundQuestionException extends Exception {
-
     public NotFoundQuestionException(String error) {
         super(error);
     }
