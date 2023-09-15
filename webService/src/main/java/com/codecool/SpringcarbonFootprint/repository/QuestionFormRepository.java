5f1c96295785049439fba072b07c17355198815b
tomatams <tomatams@gmail.com>
2023-09-15 10:02:54 +0200

    add QuestionFormRepository

---
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/repository/QuestionFormRepository.java
@@ -0,0 +1,12 @@
+package com.codecool.SpringcarbonFootprint.repository;
+
+import com.codecool.SpringcarbonFootprint.model.Question;
+import com.codecool.SpringcarbonFootprint.model.QuestionForm;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface QuestionFormRepository extends JpaRepository<QuestionForm, UUID> {
+}
