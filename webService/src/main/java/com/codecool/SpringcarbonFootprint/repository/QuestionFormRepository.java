ff0ba9c81639785ce2f911b8976e1d0c8aae360c
tomatams <tomatams@gmail.com>
2023-09-19 13:42:00 +0200

    delete unused rows from QuestionFormRepository

--- webService/src/main/java/com/codecool/SpringcarbonFootprint/repository/QuestionFormRepository.java
+++ webService/src/main/java/com/codecool/SpringcarbonFootprint/repository/QuestionFormRepository.java
@@ -1,6 +1,5 @@
 package com.codecool.SpringcarbonFootprint.repository;
 
-import com.codecool.SpringcarbonFootprint.model.Question;
 import com.codecool.SpringcarbonFootprint.model.QuestionForm;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
